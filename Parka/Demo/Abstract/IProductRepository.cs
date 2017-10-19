424af743b4b591519fdf31d2abd0018f21a3c075
Mohsen Abbasi <mabbasidesign@yahoo.com>
2017-10-19 11:45:19 -0600

    Add dependency & repository

---
+++ Parka/Demo/Abstract/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Demo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product>Products { get; }
+    }
+}
---
+++ Parka/UI/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Demo.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository repository;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ActionResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
